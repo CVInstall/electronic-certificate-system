5c760e2e427298985acf8abfd75a56fb17afb96a
LoveCodeMan <zacksemail@163.com>
2019-04-17 11:22:02 +0800

    modefy

--- src/views/login.vue
+++ src/views/login.vue
@@ -9,19 +9,33 @@
         </el-row>
 
         <el-row>
+          <!-- 输入框 start -->
           <div class="login-form">
             <div style="margin: 20px;"></div>
             <el-form :label-position="labelPosition" label-width="90px" ref="loginData" :model="loginData" :rules="loginData.rules"
               hide-required-asterisk>
               <el-form-item prop="name" label="用户名" style="margin-bottom: 30px;">
-                <el-input v-model="loginData.name"></el-input>
+                <el-input v-model="loginData.name" placeholder="Input account here" clearable></el-input>
               </el-form-item>
               <el-form-item prop="password" label="密码">
-                <el-input type="password" v-model="loginData.password" @keyup.enter.native="login('loginData')"></el-input>
+                <el-input type="password" v-model="loginData.password" placeholder="Input password here" @keyup.enter.native="login('loginData')"
+                  clearable></el-input>
               </el-form-item>
-              <el-button @click="login('loginData')" type="primary" plain>登录</el-button>
+              <el-form-item>
+                <el-button @click="login('loginData')" type="primary" class="login" plain>登录</el-button>
+              </el-form-item>
             </el-form>
           </div>
+          <!-- 输入框 end -->
+        </el-row>
+
+        <el-row class="tips">
+          <el-col :span="12">
+            <span class="words user-register" @click="register">注册</span>
+          </el-col>
+          <el-col :span="12">
+            <span class="words find-password">忘记密码</span>
+          </el-col>
         </el-row>
       </div>
     </div>
@@ -43,7 +57,7 @@ export default {
           ],
           password: [
             { required: true, message: '请输入登录密码', trigger: 'blur' },
-            { pattern: /^[A-Za-z0-9]+$/, message: '密码不能为汉字' }
+            { pattern: /^[a-zA-Z]\w{5,17}$/, message: '密码须以字母开头，长度在6~18之间' }
           ]
         }
       }
@@ -53,7 +67,7 @@ export default {
     login (formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          if (this.loginData.name === '123456' && this.loginData.password === '123456') {
+          if (this.loginData.name === '123456' && this.loginData.password === 'lzh123') {
             this.$router.push({
               path: '/upload'
             })
@@ -68,6 +82,12 @@ export default {
         }
         return false
       })
+    },
+    register () {
+      this.loginFrame.ShowLoginFrame = false
+      if (this.loginFrame.ShowLoginFrame === false) {
+        this.loginFrame.ShowRegisterFrame = true
+      }
     }
   }
 }
@@ -88,7 +108,7 @@ export default {
   bottom: 0;
   margin: auto;
   width: 30%;
-  height: 36%;
+  height: 40%;
   background: rgba(240, 255, 255, 0.4);
   box-shadow: 0 0 8px #000;
   transition: 0.5s;
@@ -126,7 +146,6 @@ export default {
   left: 10px;
   width: 50%;
   height: 20%;
-
 }
 
 @keyframes fadeInUp {
@@ -145,30 +164,46 @@ export default {
 .head {
   color: #fff;
   font-size: 20px;
-  /* font-weight: bold; */
   line-height: 50px;
   text-align: center;
   border-bottom: 1px solid #fff;
   text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
 }
 
-.el-input {
+.el-input__inner {
   width: 90%;
   background: #fdfdfd;
   border: 2px solid #dbdee1;
   box-shadow: 0 2px 2px 0 rgba(7, 0, 2, 0.25);
+}
+.el-form-item {
+  margin-right: 8%;
 }
 .el-form-item__label {
   font-size: 20px;
   font-weight: bold;
 }
 .el-button {
-  position: absolute;
-  right: 8%;
-  bottom: -30%;
-  width: 120px;
+  width: 100%;
   background: #00aaf3;
   color: #fff;
-  font-size: 18px;
+  font-size: 16px;
+}
+.words {
+  color: #d6d4d4;
+  font-size: 16px;
+  font-weight: 600;
+  text-decoration: underline;
+  text-shadow: -1px 0 rgb(36, 35, 35), 0 1px rgb(36, 35, 35), 1px 0 rgb(36, 35, 35), 0 -1px rgb(36, 35, 35);
+}
+.words:hover {
+  color: #ffce7b;
+  cursor: pointer;
+}
+.user-register {
+  margin-left: 20%;
+}
+.find-password {
+  margin-left: 60%;
 }
 </style>
--- src/views/upload.vue
+++ src/views/upload.vue
@@ -1,61 +1,44 @@
 <template>
   <div class="dispaly-style">
     <!-- 文字编辑 -->
-    <el-col :span="8">
+    <el-col :span="10">
       <div class="from-area">
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-          <el-form-item label="上传名称" prop="name">
-            <el-input v-model="ruleForm.name" clearable></el-input>
+          <el-form-item label="上传者" prop="name">
+            <el-input v-model="ruleForm.name" placeholder="如：' 张三 '" clearable></el-input>
           </el-form-item>
-          <el-form-item label="文件类型" prop="region">
-            <el-select v-model="ruleForm.region" placeholder="请选择活动区域" clearable>
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
+          <el-form-item label="文件类型" prop="type">
+            <el-input v-model="type" placeholder="请输入上传文件类型" clearable></el-input>
           </el-form-item>
-          <el-form-item label="活动时间" required>
-            <el-col :span="11">
-              <el-form-item prop="date1">
-                <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
-              </el-form-item>
-            </el-col>
-            <el-col class="line" :span="2">-</el-col>
-            <el-col :span="11">
-              <el-form-item prop="date2">
-                <el-time-picker placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
+          <el-form-item label="上传时间" required>
+            <el-col :span="12">
+              <el-form-item prop="date1" style="padding:0">
+                <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1"></el-date-picker>
               </el-form-item>
             </el-col>
           </el-form-item>
-          <el-form-item label="即时配送" prop="delivery">
+          <!-- <el-form-item label="即时配送" prop="delivery">
             <el-switch v-model="ruleForm.delivery"></el-switch>
-          </el-form-item>
-          <el-form-item label="活动性质" prop="type">
-            <el-checkbox-group v-model="ruleForm.type">
-              <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-              <el-checkbox label="地推活动" name="type"></el-checkbox>
-              <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-              <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-            </el-checkbox-group>
-          </el-form-item>
+          </el-form-item> -->
           <el-form-item label="特殊资源" prop="resource">
             <el-radio-group v-model="ruleForm.resource">
               <el-radio label="线上品牌商赞助"></el-radio>
               <el-radio label="线下场地免费"></el-radio>
             </el-radio-group>
           </el-form-item>
-          <el-form-item label="活动形式" prop="desc">
-            <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+          <el-form-item label="备注" prop="remark">
+            <el-input type="textarea" v-model="ruleForm.remark"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-            <el-button @click="resetForm('ruleForm')">重置</el-button>
+            <el-button type="info" icon="el-icon-refresh" @click="resetForm('ruleForm')">重置信息</el-button>
+            <el-button type="primary" icon="el-icon-edit" @click="submitForm('ruleForm')">立即创建</el-button>
           </el-form-item>
         </el-form>
       </div>
     </el-col>
 
     <!-- 上传图片 -->
-    <el-col :span="16" class="upload-area">
+    <el-col :span="14" class="upload-area">
       <div class="upload-area">
         <el-popover placement="top-start" title="上传区域" width="200" trigger="hover" content="点击“+”进行上传">
           <!-- <el-button slot="reference">hover 激活</el-button> -->
@@ -80,13 +63,12 @@ export default {
       dialogVisible: false,
       ruleForm: {
         name: '',
-        region: '',
+        type: '',
         date1: '',
         date2: '',
         delivery: false,
-        type: [],
         resource: '',
-        desc: ''
+        remark: ''
       },
       rules: {
         name: [
@@ -94,7 +76,7 @@ export default {
           { min: 7, max: 14, message: '长度在 3 到 5 个字符', trigger: 'blur' },
           { pattern: /^[\u4E00-\u9FA5A-Za-z0-9_]+$/, message: '事件名称不能包含特殊字符' }
         ],
-        region: [
+        type: [
           { required: true, message: '请选择活动区域', trigger: 'change' }
         ],
         date1: [
@@ -103,13 +85,13 @@ export default {
         date2: [
           { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
         ],
-        type: [
+        activity: [
           { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
         ],
         resource: [
           { required: true, message: '请选择活动资源', trigger: 'change' }
         ],
-        desc: [
+        remark: [
           { required: true, message: '请填写活动形式', trigger: 'blur' }
         ]
       }
@@ -149,9 +131,12 @@ export default {
   background-size: 100% 100%;
 }
 .from-area {
-  width: 100%;
+  color: #fff;
+  width: 90%;
   height: 100%;
-  padding: 12% 40% 5% 0;
+  margin: 5% 0 0 5%;
+  background: rgba(238, 238, 238, 0.8);
+  box-shadow: 5px 0 15px #000, 1px 0 2px #000 inset;
 }
 .upload-area {
   min-height: 770px;
@@ -160,6 +145,8 @@ export default {
   align-items: center;
 }
 .el-form-item {
-  margin-bottom: 34px;
+  color: #fff;
+  padding-top: 10px;
+  margin: 0 30px 20px 0;
 }
 </style>
